c82ec25e869d0417840dec5314706dbf242027dc
Greatness Okorie <grucookorie@gmail.com>
2026-03-14 00:28:20 +0100

    Add files via upload

---
+++ vm/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start Virtual Framebuffer
+Xvfb :1 -screen 0 $RESOLUTION &
+sleep 2
+
+# Start Desktop Environment
+startxfce4 &
+sleep 2
+
+# Start VNC Server
+x11vnc -display :1 -forever -nopw -rfbport 5900 -shared &
+
+# Start noVNC proxy
+/opt/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080
